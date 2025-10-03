6ad99543df5f6807e2a00cfbe618c427472e4a63
Bobbaiii <75533796+Bobbaiii@users.noreply.github.com>
2025-10-03 15:35:58 +0200

    Add vue-toastification integration

--- finance-app/frontend/src/main.js
+++ finance-app/frontend/src/main.js
@@ -2,6 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
 import './assets/css/tailwind.css' // Si vous utilisez Tailwind CSS
 
-createApp(App).use(router).use(store).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+app.use(store)
+app.use(Toast)
+
+app.mount('#app')
